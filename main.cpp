6735692879712e3f24a674989655710c503cf142
JC555 <572670014@qq.com>
2019-09-07 08:50:08 +0800

    麻将初级

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include "majiang.h"
+#include "player.h"
+
+int main()
+{
+	MJMontain m;
+	Player p[4] = { "wang", "zhang", "li", "zhao" };
+
+	m.createMontain();
+	m.shootCrap();
+
+	//m.outputMontain();
+
+	//cout << m;
+
+	for (int i = 0; i < 13; i++)
+	{
+		p[0].getmj(m.sendmj());
+		//p[0].showmj();
+	}
+
+	while (1)
+	{
+		p[0].getmj(m.sendmj());
+	}
+	return 0;
+}
